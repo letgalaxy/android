4f3e915f1e8639bd7a05f1db2c7187c689920639
letgalaxy <17254355+letgalaxy@users.noreply.github.com>
2019-01-11 11:52:47 +0000

    Remove jack configuration

--- build.sh
+++ build.sh
@@ -2,9 +2,7 @@ cd ~/android/lineage/
 repo sync --force-sync -q
 source build/envsetup.sh
 
+breakfast onyx
 export USE_CCACHE=1
 ccache -M 64G
-export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4G"
-
-breakfast onyx
 mka bacon
