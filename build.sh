02c120ce495823f59db8e6d11887bf22a4dff28f
letgalaxy <17254355+letgalaxy@users.noreply.github.com>
2019-01-24 00:40:17 +0000

    Add variables

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
-cd ~/android/lineage/
-repo sync --force-sync -q
-source build/envsetup.sh
-
-breakfast onyx
+WORK_DIR=~/android/lineage
+OUT_DIR=out
+LINEAGE_BUILD=onyx
 export USE_CCACHE=1
+
+cd $WORK_DIR
+repo sync --force-sync -q
+. build/envsetup.sh
+
+breakfast $LINEAGE_BUILD
 ccache -M 64G
+[ -d $OUT_DIR ] && rm -r $OUT_DIR
 mka bacon
