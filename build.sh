7b518e37585d72d334368723784adbcf1c9e6cb8
letgalaxy <17254355+letgalaxy@users.noreply.github.com>
2019-04-15 16:30:19 +0200

    Remove set -e from build.sh since 'source build/envsetup.sh' exits with a non-zero status

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
-set -e
-
+#!/bin/bash
 WORK_DIR=lineage
 LINEAGE_BUILD=onyx
 export USE_CCACHE=1
 
 cd $WORK_DIR
 repo sync --force-sync -q
-. build/envsetup.sh
+source build/envsetup.sh
 
 breakfast $LINEAGE_BUILD
 ccache -M 48G
 mka bacon
 
-cp $OUT/lineage-* ~/
+cp $OUT/lineage-* /mnt/c/Users/bhaak/OneDrive/Android
--- init.sh
+++ init.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 set -e
-
 WORK_DIR=lineage
 
 mkdir $WORK_DIR
 cd $WORK_DIR
 repo init -u git://github.com/LineageOS/android.git -b lineage-16.0
-
 mkdir .repo/local_manifests
 ln ../local_manifest.xml .repo/local_manifests/onyx.xml
