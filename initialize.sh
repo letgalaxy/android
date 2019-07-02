3c87f5962a76ac5f48a19a3b4b721ddd1b27f4ef
letgalaxy <17254355+letgalaxy@users.noreply.github.com>
2019-07-02 19:03:12 +0200

    Create README.md

--- initialize.sh
+++ initialize.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-# Package requirements for building LineageOS:
-# adb fastboot bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick
-# lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev
-# libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev repo
-
-# For building on WSL remember to set the following registry key:
-# Computer\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\kernel -> obcaseinsensitive = 0
-
 set -e
 WORK_DIR=lineage
 
