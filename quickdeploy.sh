872cdef6ff28ed46b696416919d7cad9dd46c5cd
letgalaxy <17254355+letgalaxy@users.noreply.github.com>
2019-07-02 12:28:17 +0200

    local_manifest.xml -> onyx.xml

---
+++ quickdeploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# For building on WSL remember to set the following registry key:
+# Computer\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\kernel -> obcaseinsensitive = 0
+
+set -e
+WORK_DIR=lineage
+
+mkdir $WORK_DIR
+cd $WORK_DIR
+repo init -u git://github.com/LineageOS/android.git -b lineage-16.0
+mkdir .repo/local_manifests
+ln ../*.xml .repo/local_manifests/
