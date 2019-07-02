234c89fca4f4f4e2ee81eac8ac86b95ac655946e
letgalaxy <17254355+letgalaxy@users.noreply.github.com>
2019-07-02 19:03:43 +0200

    Make if statements more condensed

--- build.sh
+++ build.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]
-then
-    echo "Usage: $0 PRODUCT"
-    exit 1
-fi
+[ "$#" == 1 ] || { echo "Usage: $0 PRODUCT"; exit 1; }
 
 export USE_CCACHE=1
 WORK_DIR=lineage
 C_SIZE=48G
-PRODUCT=$1
 
 cd $WORK_DIR
 repo sync --force-sync -q
 source build/envsetup.sh
 
 ccache -M $C_SIZE
-brunch $PRODUCT
+brunch $1
--- upload.sh
+++ upload.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ] || ! [ -d "$1" ]
-then
-    echo "Usage: $0 UPLOAD_DIR"
-    exit 1
-fi
-
-if [ ! -f $OUT/lineage-* ]
-then
-    echo "Error: No files to upload"
-    exit 1
-fi
+[ "$#" == 1 ] || [ -d "$1" ] || { echo "Usage: $0 UPLOAD_DIR"; exit 1; }
+[ -f $OUT/lineage-* ] || { echo "Error: No files to upload"; exit 1; }
 
 mv $OUT/lineage-* $1
